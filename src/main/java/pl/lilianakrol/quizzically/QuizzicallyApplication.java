11bc3058fd5934e5a4207588221d2efdea6a26fb
LilianaK4 <liliana.krol4@gmail.com>
2023-03-25 17:47:30 +0100

    initial commit

---
+++ src/main/java/pl/lilianakrol/quizzically/QuizzicallyApplication.java
@@ -0,0 +1,13 @@
+package pl.lilianakrol.quizzically;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzicallyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzicallyApplication.class, args);
+    }
+
+}
