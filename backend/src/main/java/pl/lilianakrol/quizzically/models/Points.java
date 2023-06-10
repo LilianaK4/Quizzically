82ec95134d96455bcaeadf0e8b5603b3d41dc73b
LilianaK4 <91942052+LilianaK4@users.noreply.github.com>
2023-06-10 04:16:47 +0200

    Revert "Merge pull request #13 from LilianaK4/last-changes"
    This reverts commit ef567a4ac209944e874a94c376a2f55e0f2c16fd, reversing
    changes made to a089ba47925cb9f955b82d714e43da6510ad37dc.

--- backend/src/main/java/pl/lilianakrol/quizzically/models/Points.java
+++ backend/src/main/java/pl/lilianakrol/quizzically/models/Points.java
@@ -3,8 +3,10 @@ package pl.lilianakrol.quizzically.models;
 import jakarta.persistence.ElementCollection;
 import lombok.Getter;
 import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.List;
+import java.util.Optional;
 
 @Entity
 @Setter
