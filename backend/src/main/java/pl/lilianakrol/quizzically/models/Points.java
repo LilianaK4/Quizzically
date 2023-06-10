6ece703fedcd5307d3deefe44e4a27d62c46d6bc
LilianaK4 <91942052+LilianaK4@users.noreply.github.com>
2023-06-10 04:15:20 +0200

    deleted unused imports

--- backend/src/main/java/pl/lilianakrol/quizzically/models/Points.java
+++ backend/src/main/java/pl/lilianakrol/quizzically/models/Points.java
@@ -3,10 +3,8 @@ package pl.lilianakrol.quizzically.models;
 import jakarta.persistence.ElementCollection;
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.List;
-import java.util.Optional;
 
 @Entity
 @Setter
